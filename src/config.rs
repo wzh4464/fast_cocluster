782d16a0db2ad5b7b1dd542727bcf4ba092ce0c1
wzh4464_fat <32484940+wzh4464@users.noreply.github.com>
2024-01-26 21:46:40 +0800

    Refactor dependencies and add Config struct for matrix configuration.
    - Update dependencies in `Cargo.lock`, adding `ndarray-npy` and `zip 0.5.13`.
    - Update dependencies in `Cargo.toml`, adding `ndarray-npy`.
    - Add `src/config.rs` file defining the `Config` struct for matrix configuration.
    - Implement `Config::new` constructor in `src/config.rs` to parse command line arguments.
    - Add unit test for `Config::new` in `src/config.rs`.
    - Use `Config::new` in `src/main.rs` to parse command line arguments.

---
+++ src/config.rs
@@ -0,0 +1,90 @@
+use std::fs::File;
+
+/**
+ * File: /src/config.rs
+ * Created Date: Friday, January 26th 2024
+ * Author: Zihan
+ * -----
+ * Last Modified: Friday, 26th January 2024 9:46:15 pm
+ * Modified By: the developer formerly known as Zihan at <wzh4464@gmail.com>
+ * -----
+ * HISTORY:
+ * Date      		By   	Comments
+ * ----------		------	---------------------------------------------------------
+ */
+use ndarray::Array2;
+use ndarray_npy::ReadNpyExt;
+pub struct Config {
+    // 字段定义
+    // need a matrix to init, float
+    matrix: Array2<f32>,
+    // shape of matrix
+    row:    usize,
+    col:    usize,
+    // m,n to save cluster number for rows and columns
+    m:      usize,
+    n:      usize,
+    // torlerance
+    tol:    f32,
+}
+
+impl Config {
+    /// constructor
+    ///
+    /// # Examples
+    /// ```bash
+    /// $ cargo run -- "data/matrix.npy" 2 2 0.1
+    pub fn new(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, Box<dyn std::error::Error>> {
+        // read args
+        // args:
+        // 0: program name
+        // 1: matrix path
+        // 2: m
+        // 3: n
+        // 4: tol
+        // read from npy file
+        args.next();
+        let reader = File::open(args.next().unwrap())?;
+        let matrix = Array2::<f32>::read_npy(reader)?;
+        let m = args.next().unwrap().parse::<usize>()?;
+        let n = args.next().unwrap().parse::<usize>()?;
+        let tol = args.next().unwrap().parse::<f32>()?;
+        let row = matrix.shape()[0];
+        let col = matrix.shape()[1];
+
+        Ok(Config {
+            matrix,
+            m,
+            n,
+            tol,
+            row,
+            col,
+        })
+    }
+}
+
+// cargo run -- /home/zihan/amazon_data/feature_matrix.npy 4 5 1e-4
+// Running `target/debug/fast_cocluster /home/zihan/amazon_data/feature_matrix.npy 4 5 1e-4`
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_config() {
+        let args = vec![
+            "target/debug/fast_cocluster".to_string(),
+            "/home/zihan/amazon_data/feature_matrix.npy".to_string(),
+            "4".to_string(),
+            "5".to_string(),
+            "1e-4".to_string(),
+        ];
+        let config = Config::new(args.into_iter()).unwrap();
+        assert_eq!(config.m, 4);
+        assert_eq!(config.n, 5);
+        assert_eq!(config.tol, 1e-4);
+        assert_eq!(config.row, 24000);
+        assert_eq!(config.col, 768);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![allow(unused)]
 
+use std::env::args;
+
 /**
  * File: /src/main.rs
  * Created Date: Friday January 12th 2024
  * Author: Zihan
  * -----
- * Last Modified: Friday, 26th January 2024 9:04:14 pm
+ * Last Modified: Friday, 26th January 2024 9:37:40 pm
  * Modified By: the developer formerly known as Zihan at <wzh4464@gmail.com>
  * -----
  * HISTORY:
@@ -13,6 +15,11 @@
  * ----------		------	---------------------------------------------------------
  */
 
+mod config;
+
 fn main() {
-    fast_cocluster::fake_logger();
+    let config = config::Config::new(args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
 }
